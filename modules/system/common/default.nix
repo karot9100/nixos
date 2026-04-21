71a924a0fccc7d43804e5d8d16d8c24ce6f6b634
Simon Vuylsteke <karot9100@gmail.com>
2026-04-21 22:06:59 +0200

    garbage collection

--- modules/system/common/default.nix
+++ modules/system/common/default.nix
@@ -25,6 +25,12 @@ in
     # Experimental Features
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+    nix.gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+
     # Enable terminal info
     environment.enableAllTerminfo = true;
 
