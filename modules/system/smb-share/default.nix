fca2fd924b60c1359d791b53b5cf90b110250e6a
Simon Vuylsteke <karot9100@gmail.com>
2026-04-22 19:45:19 +0200

    smb fix

--- modules/system/smb-share/default.nix
+++ modules/system/smb-share/default.nix
@@ -1,44 +1,38 @@
 { config, lib, pkgs, ... }:
 
+let
+  commonOpts = [
+    "credentials=/etc/nixos/secrets/smb/default.nix"
+    "uid=1000"
+    "gid=100"
+    "iocharset=utf8"
+    "vers=3.0"
+    "noauto"
+    "x-systemd.automount"
+    "x-systemd.idle-timeout=60"
+    "x-systemd.mount-timeout=10s"
+    "_netdev"
+  ];
+in
 {
   options.mymodules.smb-share.enable = lib.mkEnableOption "a-NAS-tasia network share";
 
   config = lib.mkIf config.mymodules.smb-share.enable {
 
     environment.systemPackages = [ pkgs.cifs-utils ];
+    boot.kernelModules = [ "cifs" ];
 
-    fileSystems."/appdata" = {
-      device = "//192.168.0.144/appdata";
+    fileSystems."/mnt/appdata" = {
+      device = "//192.168.0.143/appdata";
       fsType = "cifs";
-      options = [
-        "credentials=/etc/nixos/secrets/smb"
-        "uid=1000"
-        "gid=100"
-        "iocharset=utf8"
-        "vers=3.0"
-        "noauto"
-        "x-systemd.automount"
-        "x-systemd.idle-timeout=60"
-        "x-systemd.mount-timeout=10s"
-        "_netdev"
-      ];
+      options = commonOpts;
     };
-    
-    fileSystems."/nas" = {
-      device = "//192.168.0.144/nas";
+
+    fileSystems."/mnt/nas" = {
+      device = "//192.168.0.143/nas";
       fsType = "cifs";
-      options = [
-        "credentials=/etc/nixos/secrets/smb"
-        "uid=1000"
-        "gid=100"
-        "iocharset=utf8"
-        "vers=3.0"
-        "noauto"
-        "x-systemd.automount"
-        "x-systemd.idle-timeout=60"
-        "x-systemd.mount-timeout=10s"
-        "_netdev"
-      ];
+      options = commonOpts;
+    };
 
   };
 }
